bcb4fcdbe33d0dae42d412423c55b91dba9a79db
Emanoel Barreiros <emanoelbarreiros@gmail.com>
2020-11-16 22:15:49 -0300

    adicionando o ponto mais próximo do círculo

--- colisao/colisao_circ_rect.py
+++ colisao/colisao_circ_rect.py
@@ -10,42 +10,47 @@ class Colisao:
         self.altura = 40
         self.raio = 10
         self.colidiu = False
+        self.x_prox = -1
+        self.y_prox = -1
         pyxel.run(self.update, self.draw)
 
     def update(self):
-        self.colidiu = self.detectar_colisao(self.x_rect, self.y_rect, self.largura, self.largura, pyxel.mouse_x, pyxel.mouse_y, self.raio)
+        self.colidiu, self.x_prox, self.y_prox = self.detectar_colisao(self.x_rect, self.y_rect, self.largura, self.largura, pyxel.mouse_x, pyxel.mouse_y, self.raio)
 
     def draw(self):
         pyxel.cls(6)
         pyxel.rect(self.x_rect, self.y_rect, self.largura, self.altura, 5)
         
         cor = 15
+        cor_ponto = 8
         if self.colidiu:
             cor = 8
+            cor_ponto = 10
         
         pyxel.circ(pyxel.mouse_x, pyxel.mouse_y, self.raio, cor)
+        pyxel.circ(self.x_prox, self.y_prox, 1, cor_ponto)
         
 
     def detectar_colisao(self, x1, y1, l1, a1, x2, y2, r):
-        teste_x = x2
-        teste_y = y2
+        x_ponto = x2
+        y_ponto = y2
         if x2 < x1:
-            teste_x = x1
+            x_ponto = x1
         elif x2 > x1 + l1:
-            teste_x = x1 + l1
+            x_ponto = x1 + l1
         
         if y2 < y1:
-            teste_y = y1
+            y_ponto = y1
         elif y2 > y1 + a1:
-            teste_y = y1 + a1
+            y_ponto = y1 + a1
 
-        dist_x = x2 - teste_x
-        dist_y = y2 - teste_y
+        dist_x = x2 - x_ponto
+        dist_y = y2 - y_ponto
         distancia = math.sqrt(dist_x**2 + dist_y**2)
 
         if distancia <= r:
-            return True
+            return True, x_ponto, y_ponto
 
-        return False
+        return False, x_ponto, y_ponto
 
 Colisao()
